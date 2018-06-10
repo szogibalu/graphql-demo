1bbb9a01e8e87aa889d8999dbd4500b079ea2609
Balázs Szögi <szogibalu@gmail.com>
2018-06-10 08:53:58 +0200

    Modified client

--- misc/grapql-demo-client/e2e/src/app.e2e-spec.ts
+++ misc/grapql-demo-client/e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to grapql-demo-client!');
+    expect(page.getParagraphText()).toEqual('GraphQL Demo Client');
   });
 });
